7957cbbcc2f5e40467b3a64e76945f48442d326a
Abhilash <abhilash.kumar.bxr@gmail.com>
2022-09-29 18:37:16 +0530

    Abhilash

--- src/main/java/com/customer/profile/registration/model/CustomerProfile.java
+++ src/main/java/com/customer/profile/registration/model/CustomerProfile.java
@@ -1,93 +1,115 @@
 package com.customer.profile.registration.model;
 
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-//@Entity
-public class CustomerProfile{
+@Entity
+public class CustomerProfile {
+
 	
-	//@Id
-	//@GeneratedValue(strategy=GenerationType.AUTO)
+	 @Id 
+	 @GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer id;
-    public String name;
-    public String username;
-    public String password;
-    public String address;
-    public String state;
-    public String country;
-    public String emailAddress;
-    public String pan;
-    public String dateOfBirth;
-    public String accountType;
-    public String MobileNo;
-    
-    public String getName() {
+	public String name;
+	public String username;
+	public String password;
+	public String address;
+	public String state;
+	public String country;
+	public String emailAddress;
+	public String pan;
+	public String dateOfBirth;
+	public String accountType;
+	public String MobileNo;
+
+	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getState() {
 		return state;
 	}
+
 	public void setState(String state) {
 		this.state = state;
 	}
+
 	public String getCountry() {
 		return country;
 	}
+
 	public void setCountry(String country) {
 		this.country = country;
 	}
+
 	public String getEmailAddress() {
 		return emailAddress;
 	}
+
 	public void setEmailAddress(String emailAddress) {
 		this.emailAddress = emailAddress;
 	}
+
 	public String getPan() {
 		return pan;
 	}
+
 	public void setPan(String pan) {
 		this.pan = pan;
 	}
+
 	public String getDateOfBirth() {
 		return dateOfBirth;
 	}
+
 	public void setDateOfBirth(String dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
+
 	public String getAccountType() {
 		return accountType;
 	}
+
 	public void setAccountType(String accountType) {
 		this.accountType = accountType;
 	}
+
 	public String getMobileNo() {
 		return MobileNo;
 	}
+
 	public void setMobileNo(String mobileNo) {
 		MobileNo = mobileNo;
 	}
-	
+
 }
